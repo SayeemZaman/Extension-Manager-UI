3bfa78b61b6d98f33ab46c200cc54f755a87ed94
sayeemzaman3 <sayeem312@gmail.com>
2025-08-04 19:29:50 +0000

    add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
-import Filter from "./Filter";
+import ThemeChanger from "./ThemeChanger";
+import logo from "../assets/images/logo.svg";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Manager Logo" />
+      <p className="title">Extensions</p>
+      <ThemeChanger />
+    </header>
+  );
+}
