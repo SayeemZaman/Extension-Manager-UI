92746e828876aa85d619e776127348ecd458fa4f
sayeemzaman3 <sayeem312@gmail.com>
2025-08-04 23:50:39 +0000

    update data.json for image issue

---
+++ src/data.js
@@ -0,0 +1,93 @@
+import img1 from "../assets/images/logo-devlens.svg";
+import img2 from "../assets/images/logo-style-spy.svg";
+import img3 from "../assets/images/logo-speed-boost.svg";
+import img4 from "../assets/images/logo-json-wizard.svg";
+import img5 from "../assets/images/logo-tab-master-pro.svg";
+import img6 from "../assets/images/logo-viewport-buddy.svg";
+import img7 from "../assets/images/logo-markup-notes.svg";
+import img8 from "../assets/images/logo-grid-guides.svg";
+import img9 from "../assets/images/logo-pallette-picker.svg";
+import img10 from "../assets/images/logo-links-checker.svg";
+import img11 from "../assets/images/logo-dom-snapshot.svg";
+import img12 from "../assets/images/logo-console-plus.svg";
+
+export const data = [
+  {
+    logo: img1,
+    name: "DevLens",
+    description:
+      "Quickly inspect page layouts and visualize element boundaries.",
+    isActive: true,
+  },
+  {
+    logo: img2,
+    name: "StyleSpy",
+    description: "Instantly analyze and copy CSS from any webpage element.",
+    isActive: true,
+  },
+  {
+    logo: img3,
+    name: "SpeedBoost",
+    description: "Optimizes browser resource usage to accelerate page loading.",
+    isActive: false,
+  },
+  {
+    logo: img4,
+    name: "JSONWizard",
+    description:
+      "Formats, validates, and prettifies JSON responses in-browser.",
+    isActive: true,
+  },
+  {
+    logo: img5,
+    name: "TabMaster Pro",
+    description: "Organizes browser tabs into groups and sessions.",
+    isActive: true,
+  },
+  {
+    logo: img6,
+    name: "ViewportBuddy",
+    description:
+      "Simulates various screen resolutions directly within the browser.",
+    isActive: false,
+  },
+  {
+    logo: img7,
+    name: "Markup Notes",
+    description:
+      "Enables annotation and notes directly onto webpages for collaborative debugging.",
+    isActive: true,
+  },
+  {
+    logo: img8,
+    name: "GridGuides",
+    description:
+      "Overlay customizable grids and alignment guides on any webpage.",
+    isActive: false,
+  },
+  {
+    logo: img9,
+    name: "Palette Picker",
+    description: "Instantly extracts color palettes from any webpage.",
+    isActive: true,
+  },
+  {
+    logo: img10,
+    name: "LinkChecker",
+    description: "Scans and highlights broken links on any page.",
+    isActive: true,
+  },
+  {
+    logo: img11,
+    name: "DOM Snapshot",
+    description: "Capture and export DOM structures quickly.",
+    isActive: false,
+  },
+  {
+    logo: img12,
+    name: "ConsolePlus",
+    description:
+      "Enhanced developer console with advanced filtering and logging.",
+    isActive: true,
+  },
+];
