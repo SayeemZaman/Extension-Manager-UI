bb98ab3a7e3905bdb07bd0054b796769c3f22f36
sayeemzaman3 <sayeem312@gmail.com>
2025-08-05 10:06:35 +0000

    add state list of extensions

--- src/components/ExtensionList.jsx
+++ src/components/ExtensionList.jsx
@@ -1,12 +1,23 @@
 import Extension from "./Extension";
 import { data } from "../data.js";
+import { useState } from "react";
 
 export default function ExtensionList() {
-  return (
-    <div id="list">
-      {data.map((ext, index) => {
-        <Extension name={ext.name} logo={ext.logo} desc={ext.desc} />;
-      })}
-    </div>
-  );
+  const [extList, setExtList] = useState([{}]);
+
+  function setExt(ext) {
+    setExtList((prev) => [
+      ...prev,
+      {
+        logo: ext.logo,
+        name: ext.name,
+        desc: ext.description,
+        isActive: ext.isActive,
+      },
+    ]);
+
+    return <Extension logo />;
+  }
+
+  return <div id="list">{data.map((ext, index) => setExt(ext))}</div>;
 }
