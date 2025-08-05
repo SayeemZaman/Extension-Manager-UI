8e9d671837cbbc4c3d2d0e42e618283886bdfbce
sayeemzaman3 <sayeem312@gmail.com>
2025-08-05 12:09:29 +0000

    add ThemeChanger button logic

--- src/components/ThemeChanger.jsx
+++ src/components/ThemeChanger.jsx
@@ -1,10 +1,14 @@
 import light from "../assets/images/icon-sun.svg";
-import moon from "../assets/images/icon-moon.svg";
+import dark from "../assets/images/icon-moon.svg";
+import { useContext } from "react";
+import { Theme } from "../contexts/theme";
 
 export default function ThemeChanger() {
+  const { theme, setTheme } = useContext(Theme);
+
   return (
-    <button id="themeChanger">
-      <img src={light} />
+    <button id="themeChanger" onClick={() => setTheme(!theme)}>
+      <img src={theme == "light" ? light : dark} alt="Theme Changer Button" />
     </button>
   );
 }
