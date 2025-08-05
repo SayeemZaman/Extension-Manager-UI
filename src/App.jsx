67690312f22183b151692e019aae9e1225a38541
sayeemzaman3 <sayeem312@gmail.com>
2025-08-05 10:48:11 +0000

    add Theme context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./components/Header.jsx";
 import "./components/ExtContainer.jsx";
+import { Theme } from "../contexts/theme.jsx";
+import { useState } from "react";
 
 export default function App() {
+  const [theme, setTheme] = useState(light);
+
   return (
     <>
-      <Header />
-      <ExtContainer />
+      <Theme.Provider value={{ theme, setTheme }}>
+        <Header />
+        <ExtContainer />
+      </Theme.Provider>
     </>
   );
 }
