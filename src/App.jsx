5cfbaa3116a242b960c378010953b780f3a3e221
sayeemzaman3 <sayeem312@gmail.com>
2025-08-05 20:24:38 +0000

    add theme className to App

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { Theme } from "../contexts/theme.jsx";
 import { useState } from "react";
 
 export default function App() {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = useState("light");
 
   return (
-    <>
+    <div className={`${theme} "App"`}>
       <Theme.Provider value={{ theme, setTheme }}>
         <Header />
         <ExtContainer />
       </Theme.Provider>
-    </>
+    </div>
   );
 }
