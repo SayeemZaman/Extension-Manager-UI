bf57d3ae56a3d0edf5fb10b8991fc95949000b09
sayeemzaman3 <sayeem312@gmail.com>
2025-08-06 19:20:17 +0000

    fix a weird error

--- src/components/ExtensionList.jsx
+++ src/components/ExtensionList.jsx
@@ -3,22 +3,14 @@ import { data } from "../data.js";
 import { useState } from "react";
 
 export default function ExtensionList() {
-<<<<<<< HEAD
   const [extList, setExtList] = useState([{}]);
 
   function setExt(ext) {
-    setExtList((prev) => [
-=======
-  const [extensionList, setExtensionList] = useState([{}]);
-
-  function setExt(ext) {
     setExtensionList((prev) => [
->>>>>>> afb7e0a1a517767b5ad7276e607a24077ef83285
       ...prev,
       {
         logo: ext.logo,
         name: ext.name,
-<<<<<<< HEAD
         desc: ext.description,
         isActive: ext.isActive,
       },
@@ -35,13 +27,4 @@ export default function ExtensionList() {
   }
 
   return <div id="list">{data.map((ext, index) => setExt(ext))}</div>;
-=======
-        description: ext.description,
-        isActive: ext.isActive,
-      },
-    ]);
-  }
-
-  return <div id="list">{data.map((ext, index) => {})}</div>;
->>>>>>> afb7e0a1a517767b5ad7276e607a24077ef83285
 }
