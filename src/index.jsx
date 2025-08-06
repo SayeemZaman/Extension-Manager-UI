92f66f092b98b6395b0adf143cff4ea76c69d112
sayeemzaman3 <sayeem312@gmail.com>
2025-08-06 00:13:18 +0000

    render App as default

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import Header from "./components/Header";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Header />
+    <App />
   </StrictMode>
 );
