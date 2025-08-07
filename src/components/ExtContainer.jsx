4b947fe530eee55b84da6a9676e3032751b624a2
SayeemZaman3 <sayeem312@gmail.com>
2025-08-07 12:17:17 +0600

    pass currentFilter state to Filter

--- src/components/ExtContainer.jsx
+++ src/components/ExtContainer.jsx
@@ -1,13 +1,16 @@
 import ExtensionList from "./ExtensionList";
 import Filter from "./Filter";
+import { useState } from 'react';
 
 export default function ExtContainer() {
+  const [currentFilter, setCurrentFilter] = useState('All')
   const filters = ["All", "Active", "Inactive"];
+  
   return (
     <>
       <div>
         <p>Extension List</p>
-        <div id="filterContainer">{filters.map((filter, index) => (<Filter key={index} name={filter} />))}</div>
+        <div id="filterContainer">{filters.map((filter, index) => (<Filter key={index} name={filter} current={currentFilter} />))}</div>
       </div>
       <ExtensionList />
     </>
