6d69f743e5ffcb393e19e3b41fcb181bd8172053
sayeemzaman3 <sayeem312@gmail.com>
2025-08-08 18:26:58 +0000

    add indenting to ExtContainer

--- src/components/ExtContainer.jsx
+++ src/components/ExtContainer.jsx
@@ -1,16 +1,25 @@
 import ExtensionList from "./ExtensionList";
 import Filter from "./Filter";
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function ExtContainer() {
-  const [currentFilter, setCurrentFilter] = useState('All')
+  const [currentFilter, setCurrentFilter] = useState("All");
   const filters = ["All", "Active", "Inactive"];
-  
+
   return (
     <>
       <div>
         <p>Extension List</p>
-        <div id="filterContainer">{filters.map((filter, index) => (<Filter key={index} name={filter} current={currentFilter} setCurrent={setCurrentFilter} />))}</div>
+        <div id="filterContainer">
+          {filters.map((filter, index) => (
+            <Filter
+              key={index}
+              name={filter}
+              current={currentFilter}
+              setCurrent={setCurrentFilter}
+            />
+          ))}
+        </div>
       </div>
       <ExtensionList />
     </>
