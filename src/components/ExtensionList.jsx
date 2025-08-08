967611c2cd0355719a3ba13686e6ff66c3df3be5
sayeemzaman3 <sayeem312@gmail.com>
2025-08-08 18:27:52 +0000

    fix Blank screen error due to infinite rendering in ExtensionList

--- src/components/ExtensionList.jsx
+++ src/components/ExtensionList.jsx
@@ -3,35 +3,25 @@ import { data } from "../data.js";
 import { useState } from "react";
 
 export default function ExtensionList() {
-  const [extList, setExtList] = useState([{}]);
-
-  function setExt(ext, id) {
-    setExtensionList((prev) => [
-      ...prev,
-      {
-        id: id,
-        logo: ext.logo,
-        name: ext.name,
-        desc: ext.description,
-        isActive: ext.isActive,
-      },
-    ]);
-
-    return (
-      <Extension
-        id={id}
-        name={ext.name}
-        desc={ext.description}
-        logo={ext.logo}
-        isActive={ext.isActive}
-        remove={remove}
-      />
-    );
-  }
+  const [extList, setExtList] = useState(data); // set initial state to data
 
   function remove(idToRemove) {
-    setExtList((prev) => prev.filter((id) => id !== idToRemove));
+    setExtList((prev) => prev.filter((_, idx) => idx !== idToRemove));
   }
 
-  return <div id="list">{data.map((ext, index) => setExt(ext, index))}</div>;
+  return (
+    <div id="list">
+      {extList.map((ext, index) => (
+        <Extension
+          key={index}
+          id={index}
+          name={ext.name}
+          desc={ext.description}
+          logo={ext.logo}
+          isActive={ext.isActive}
+          remove={remove}
+        />
+      ))}
+    </div>
+  );
 }
