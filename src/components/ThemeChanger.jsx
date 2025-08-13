156764e1ad2d5c1cae15769c0eb2b6b66ef348d6
sayeemzaman3 <sayeem312@gmail.com>
2025-08-13 23:32:09 +0000

    fix Theme switching

--- src/components/ThemeChanger.jsx
+++ src/components/ThemeChanger.jsx
@@ -7,7 +7,10 @@ export default function ThemeChanger() {
   const { theme, setTheme } = useContext(Theme);
 
   return (
-    <button id="themeChanger" onClick={() => setTheme(!theme)}>
+    <button
+      id="themeChanger"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
       <img src={theme == "light" ? light : dark} alt="Theme Changer Button" />
     </button>
   );
